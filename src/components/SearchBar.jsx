0cc33a2d1744d5223a49cf423cee9bc548101391
Kunal <kdshah6593@gmail.com>
2021-09-03 16:37:07 -0400

    removed unused css; set up fetch request for data

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,22 +6,16 @@ import SearchIcon from '@material-ui/icons/Search';
 const useStyles = makeStyles((theme) => ({
     search: {
         display: 'flex',
-        // position: 'relative',
         borderRadius: theme.shape.borderRadius,
         backgroundColor: theme.palette.common.white,
         '&:hover': {backgroundColor: theme.palette.common.white},
         marginRight: theme.spacing(2),
         marginLeft: theme.spacing(2),
         width: '100%',
-        // [theme.breakpoints.up('sm')]: {
-        //   marginLeft: theme.spacing(3),
-        //   width: 'auto',
-        // },
       },
       searchIcon: {
         padding: theme.spacing(2, 2),
         height: '100%',
-        // position: 'absolute',
         pointerEvents: 'none',
         display: 'flex',
         alignItems: 'center',
@@ -36,9 +30,6 @@ const useStyles = makeStyles((theme) => ({
         // vertical padding + font size from searchIcon
         paddingLeft: theme.spacing(0),
         width: '100%',
-        // [theme.breakpoints.up('sm')]: {
-        //   width: '100ch',
-        // },
       },
       btn: {
         backgroundColor: "#E09F3E",
@@ -55,6 +46,7 @@ const SearchBar = () => {
     const classes = useStyles();
     const baseUrl = "https://thestormlightapi.com/api/";
     const [searchText, setSearchText] = useState(baseUrl);
+    const [searchResults, setSearchResults] = useState({});
 
     const handleSearchChange = (e) => {
         setSearchText(e.target.value)
@@ -63,6 +55,11 @@ const SearchBar = () => {
     const handleSearchSubmit = (e) => {
         e.preventDefault();
         console.log("Form Submitted")
+        fetch(searchText)
+        .then(resp => resp.json())
+        .then(data => {
+            setSearchResults(data);
+        })
     }
 
     return (
