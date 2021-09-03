fccde98799bfc93d769192ffc6abadc5462f0828
Kunal <kdshah6593@gmail.com>
2021-09-03 16:31:33 -0400

    update styles for header, update components in home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,8 +14,12 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 0,
   },
+  navHeadings: {
+    flexGrow: 1,
+    textAlign: 'right',
+  }
 }));
 
 export default function ButtonAppBar() {
@@ -27,15 +29,14 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             The Stormlight API
           </Typography>
-          <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}><Button color="inherit">Home</Button></Link>
-          <Link to="/about" style={{ color: 'inherit', textDecoration: 'inherit'}}><Button color="inherit">About</Button></Link>
-          <Link to="/documentation" style={{ color: 'inherit', textDecoration: 'inherit'}}><Button color="inherit">Documentation</Button></Link>
+          <Typography className={classes.navHeadings}>
+            <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}><Button color="inherit">Home</Button></Link>
+            <Link to="/about" style={{ color: 'inherit', textDecoration: 'inherit'}}><Button color="inherit">About</Button></Link>
+            <Link to="/documentation" style={{ color: 'inherit', textDecoration: 'inherit'}}><Button color="inherit">Documentation</Button></Link>
+          </Typography>
         </Toolbar>
       </AppBar>
     </div>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from '../components/Header'
+import SearchDataContainer from '../containers/SearchDataContainer';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }
+}));
 
 const HomePage = () => {
+
+    const classes = useStyles();
 
     return (
         <div>
             <Header/>
-            <p>Home Page</p>
+            <div className={classes.root}>
+                <h1>Welcome to the Stormlight API</h1>
+                <h3>Find all the data from the world of Roshar</h3>
+                <SearchDataContainer />
+            </div>
         </div>
     )
 }
