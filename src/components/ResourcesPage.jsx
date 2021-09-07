fe5fbc02c60b2616475658a07835065216c61a37
Kunal <kdshah6593@gmail.com>
2021-09-07 17:57:47 -0400

    set up the same to overview page with linked tabs

--- src/components/ResourcesPage.jsx
+++ src/components/ResourcesPage.jsx
@@ -1,42 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
-const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
 
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
     'aria-controls': `vertical-tabpanel-${index}`,
   };
+}
+
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      {...props}
+    />
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -46,9 +27,22 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
   },
   tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`,
+    // borderRight: `1px solid ${theme.palette.divider}`,
     overflow: 'visible',
+    width: '200px',
+    position: 'fixed',
+    // zIndex: 1,
+    // overflowX: 'hidden',
   },
+  sections: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingLeft: 20,
+    marginLeft: '200px',
+  },
+  tab: {
+    border: `1px solid ${theme.palette.divider}`,
+  }
 }));
 
 const ResourcesPage = () => {
@@ -64,40 +58,26 @@ const ResourcesPage = () => {
       <Tabs
         orientation="vertical"
         variant="scrollable"
+        indicatorColor="#fff"
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
         className={classes.tabs}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
+        <LinkTab label="Root" href="#" {...a11yProps(0)} className={classes.tab} />
+        <LinkTab label="Books" href="#" {...a11yProps(1)} className={classes.tab} />
+        <LinkTab label="Characters" href="#" {...a11yProps(2)} className={classes.tab} />
+        <LinkTab label="Essences" href="#" {...a11yProps(3)} className={classes.tab} />
+        <LinkTab label="Surges" href="#" {...a11yProps(4)} className={classes.tab} />
+        <LinkTab label="Order of the Knights Radiant" href="#" {...a11yProps(5)} className={classes.tab} />
+        <LinkTab label="Heralds" href="#" {...a11yProps(6)} className={classes.tab} />
+        <LinkTab label="Nations" href="#" {...a11yProps(7)} className={classes.tab} />
+        <LinkTab label="Races" href="#" {...a11yProps(8)} className={classes.tab} />
       </Tabs>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+
+      <div className={classes.sections}>
+
+      </div>
     </div>
   );
 }
