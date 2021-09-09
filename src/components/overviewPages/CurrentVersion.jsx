77201d28304c6efc5a12a3bac6b4d1d5304966f2
Kunal <kdshah6593@gmail.com>
2021-09-09 17:53:58 -0400

    test tab anchor links to header titles for sections

--- src/components/overviewPages/CurrentVersion.jsx
+++ src/components/overviewPages/CurrentVersion.jsx
@@ -20,10 +20,13 @@ const CurrentVersion = (props) => {
     const classes = useStyles();
     
     return (
+        <>
+        <div id={props.id} ></div>
         <div className={classes.root}>
-          <h2 id={props.id} className={classes.title}>Current Version</h2>
+          <h2 className={classes.title}>Current Version</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
+        </>
     )
 }
 
