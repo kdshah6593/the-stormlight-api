b4f4c182384f3cdfefea32176e482505b132fe48
Kunal <kdshah6593@gmail.com>
2021-09-09 16:59:00 -0400

    add background image, add infocontainer and footer, adjust styling of text

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import Header from '../components/Header'
-import SearchDataContainer from '../containers/SearchDataContainer';
 import { makeStyles } from '@material-ui/core/styles';
+import Header from '../components/Header';
+import SearchDataContainer from '../containers/SearchDataContainer';
+import InformationContainer from '../containers/InformationContainer';
+import BackgroundImage from '../images/wayofkings.jpg';
+import Footer from '../components/Footer';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
+      backgroundImage: `url(${BackgroundImage})`,
+      backgroundSize: 'cover',
     },
     title: {
         fontFamily: "'Cinzel Decorative', cursive",
@@ -20,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
         fontFamily: "'Cinzel Decorative', cursive",
         letterSpacing: "5px",
         fontSize: "30px",
-        marginTop: "1%",
+        marginTop: 0,
         marginBottom: "1%",
     }
 }));
@@ -35,10 +40,12 @@ const HomePage = () => {
         <div>
             <Header/>
             <div className={classes.root}>
-                <h1 className={classes.title}>Welcome to th<span style={{marginLeft: "17px"}}>e</span> Stormlight API</h1>
+                <h1 className={classes.title}>Welcome to th{e} Stormlight API</h1>
                 <h3 className={classes.subTitle}>Find all th{e} data from th{e} world of Roshar</h3>
                 <SearchDataContainer />
+                <InformationContainer />
             </div>
+            <Footer />
         </div>
     )
 }
