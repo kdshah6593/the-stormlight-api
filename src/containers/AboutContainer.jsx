aca06f153faf38333b157afb1800b821419b06cd
Kunal <kdshah6593@gmail.com>
2021-09-16 19:10:28 -0400

    set up bullet point list using flexbox with icons; changed text content of page

--- src/containers/AboutContainer.jsx
+++ src/containers/AboutContainer.jsx
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Link } from '@material-ui/core';
 import Header from '../components/Header'
 import Footer from '../components/Footer';
+import FlareIcon from '@material-ui/icons/Flare';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import ComputerIcon from '@material-ui/icons/Computer';
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -24,6 +27,15 @@ const useStyles = makeStyles((theme) => ({
         fontFamily: "'Cinzel Decorative', cursive",
         letterSpacing: "5px",
         margin: theme.spacing(2,0,2,0),
+    },
+    techList: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    techListItem: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
     }
 }));
 
@@ -39,17 +51,22 @@ const AboutContainer = () => {
                 <p>This API is a source for structured data from the Stormlight Archive universe. I give you access to data about all the Books, Characters, and more in an easy to use JSON format.</p>
 
                 <h2 className={classes.subHeader}>Who am I?</h2>
-                <p>I'm Kunal and I am a full stack web developer. My friend introduced to me to this series and I loved immersing myself in this world. Inspired by the The Game of Thrones API, I wanted to built this for other developers that enjoy the Stormlight Archive. If you have questions or thoughts on how to improve this API, please send me an email.</p>
+                <p>I'm Kunal and I am a full stack web developer. My friend introduced to me to this series and I loved immersing myself in this world. Inspired by <Link href="https://anapioficeandfire.com/" target="_blank" rel="noopener" color="error">An API of Ice and Fire</Link>, the popular Game of Thrones API, I wanted to build this for other developers that enjoy the Stormlight Archive. If you have questions or thoughts on how to improve this API, please send me an email.</p>
 
                 <h2 className={classes.subHeader}>Technologies</h2>
-                <ul>
-                    <li>Rails API</li>
-                    <li>React.js</li>
-                </ul>
+                <p>This API was built using various technologies.</p>
+                <div className={classes.techList}>
+                    <div className={classes.techListItem}><ComputerIcon style={{marginRight: '10px'}}/><p><strong>Ruby on Rails</strong> - The framework that the API/backend was built on.</p></div>
+                    <div className={classes.techListItem}><ComputerIcon style={{marginRight: '10px'}}/><p><strong>PostgreSQL</strong> - The object-relational database system used to manage all the data.</p></div>
+                    <div className={classes.techListItem}><ComputerIcon style={{marginRight: '10px'}}/><p><strong>React.js</strong> - JavaScript library used to build the frontend.</p></div>
+                    <div className={classes.techListItem}><ComputerIcon style={{marginRight: '10px'}}/><p><strong>Material-UI</strong> - React UI Framework used to design the frontend.</p></div>
+                    <div className={classes.techListItem}><ComputerIcon style={{marginRight: '10px'}}/><p><strong>Firebase</strong> - hosts the frontend page that are interacting with.</p></div>
+                </div>
 
                 <h2 className={classes.subHeader}>Can You Contribute?</h2>
-                <p>Yes, I need the help! If you create a username and password, you will be able to add or edit data. After making 5 contributions, you will gain full editing access that doesn't require review by me.</p>
-                <p>If you want to help with the actual frontend or backend of the website, check out the contributing page for instructions.</p>
+                <p>Yes, I need the help! If you click here: Add/Edit, it will direct you to a form page to add new data or edit preexisting data.</p>
+                <p>The world of Roshar is huge and constantly needs updating. If you add or edit data, I ask that you please contribute with relevant information. Currently, I am the only one managing it and I don't always have time to review that all the information is correct. Currently, since I am trying to grow the database, I am allowing anyone to add or edit the data without review. There may be developers that use this information for their own projects, so I ask that do not purposefully put false data in the database. I am in the process of creating the ability to sign up as a verified contributor to eliminate the possibility of incorrect data being added.</p>
+                <p>If you want to help with the actual frontend or backend of the website, check out the Github documentation's contributing page for instructions.</p>
 
                 <h2 className={classes.subHeader}>Copyright</h2>
                 <p>
