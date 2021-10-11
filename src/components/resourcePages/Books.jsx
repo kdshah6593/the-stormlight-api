3f2035b7ee055f462bbe9016e75be682752ca6f4
Kunal <kdshah6593@gmail.com>
2021-10-11 17:28:00 -0400

    testing changes to text format for code example; add table to display attributes

--- src/components/resourcePages/Books.jsx
+++ src/components/resourcePages/Books.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -24,11 +31,25 @@ const useStyles = makeStyles((theme) => ({
       fontFamily: "'Montserrat', sans-serif",
       fontWeight: '500',
       fontSize: '20px',
-  }
+    },
+    table: {
+      minWidth: 650,
+    },
 }));
 
 const Books = (props) => {
     const classes = useStyles();
+
+    function createData(name, type, description) {
+      return { name, type, description };
+    }
+
+    const rows = [
+      createData('Title', "string", "description"),
+      createData('Author', "string", "description"),
+      createData('ISBN', "string", "description"),
+      createData('Pages', "integer", "description"),
+    ];
     
     return (
       <>
@@ -36,22 +57,50 @@ const Books = (props) => {
         <div className={classes.root} id={props.id}>
           <h2 className={classes.title}>Books</h2>
           <p className={classes.content}>Example Request</p>
-          <code>{"https://www.anapioficeandfire.com/api/v1/books"}</code>
+          <code>"https://www.anapioficeandfire.com/api/v1/books"</code>
           <p className={classes.content}>Example Response</p>
-          <code>{`
-            {
-              "data": [
-                {
-                  "title": "The Way of Kings",
-                  "author": "Brandon Sanderson",
-                },
-                {
-                  "title": "Words of Radiance",
-                  "author": "Brandon Sanderson",
-                },
-              ]
-            }  
-          `}</code>
+          <code>
+            {`
+              {
+                "data": [
+                  {
+                    "title": "The Way of Kings",
+                    "author": "Brandon Sanderson",
+                    ...
+                  },
+                  {
+                    "title": "Words of Radiance",
+                    "author": "Brandon Sanderson",
+                    ...
+                  },
+                ]
+              }  
+            `}
+          </code>
+
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right">Type</TableCell>
+                  <TableCell align="right">Description</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.type}</TableCell>
+                    <TableCell align="right">{row.description}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
           <p className={classes.content}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </>
